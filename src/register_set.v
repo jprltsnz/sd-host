5a478bbf714865ffc4a2f893d3de2c81df155cf6
Javier Peralta <javinachop@gmail.com>
2016-11-12 21:12:40 -0600

    Rework to function as standrad dictates
    The past iteration had bugs on the errors on the offset for address and
    number of registers was wrong, the code was reworked to function
    properly also it was refactored to uphold more modern verilog standards.

--- src/register_set.v
+++ src/register_set.v
@@ -1,63 +1,53 @@
-module register_set(
-                    clk, //clock signal
-                    reset, // reset active on high
-                    req, // request read/write
-                    ack, // acknowlage that the memory operation is done
-                    address, // address of data to read/write
-                    wnr, // if 0 operation is read, if 1 operation is write
-                    data_in, // data to be written
-                    data_out, // read data
-                    mem_data_out // data of all registers
-                    );
+module register_set #(
+                     parameter DATA_WIDTH = 8,
+                     parameter ADDR_WIDTH = 8,
+                     parameter MEM_DEPTH = 1 << ADDR_WIDTH
+                     )
+                     (
+                      input logic                             clk,
+                      input logic                             reset,
+                      input logic                             wnr,
+                      input logic                             req,
+                      input logic [ADDR_WIDTH-1:0]            address,
+                      input logic [DATA_WIDTH-1:0]            data_in,
 
-   parameter DATA_WIDTH = 16;
-   parameter ADDR_WIDTH = 16;
-   parameter MEM_DEPTH = 1 << ADDR_WIDTH;
+                    //Output ports
+                      output logic                            ack,
+                      output logic [DATA_WIDTH-1:0]           data_out,
+                      output logic [DATA_WIDTH*MEM_DEPTH-1:0] mem_data_out
+                                 );
 
+   reg [DATA_WIDTH - 1: 0]    mem [0 : MEM_DEPTH - 1];
 
-   // Input ports
-   input clk, reset, wnr;
-   input req;
-   input [ADDR_WIDTH-1:0] address;
-   input [DATA_WIDTH-1:0] data_in;
+//   integer                    j;
+   genvar                     i;
 
-   //Output ports
-   output                 ack;
-   output [DATA_WIDTH-1:0] data_out;
-   output [DATA_WIDTH*MEM_DEPTH-1:0] mem_data_out;
-
-   // Variable types
-   logic                             clk, reset, wnr, ack;
-   logic [DATA_WIDTH-1:0]            data_in, data_out;
-   logic [ADDR_WIDTH-1:0]            address;
-   logic [DATA_WIDTH*MEM_DEPTH-1:0]  mem_data_out;
-   logic [DATA_WIDTH-1:0]            mem [0:MEM_DEPTH-1];
 
    for(i=0; i<MEM_DEPTH;i=i+1) begin
       assign mem_data_out[DATA_WIDTH*i+:DATA_WIDTH] = mem[i];
    end
 
    always @(posedge clk) begin
-      if(reset) begin
-         for(i = 0; i < MEM_DEPTH; i = i+1) begin
-            mem[i] <= 0;
+/*     if(reset) begin
+         for(j = 0; j < MEM_DEPTH; j = j+1) begin
+            mem[j] <= 0;
+         end
+      end */
+      ack = 0;
+      data_out = 0;
+
+      if(wnr) begin
+         if(req == 1) begin
+            mem[address] = data_in;
+            ack = 1;
+         end
+      end
+      else begin
+         if (req == 1) begin
+         data_out = mem[address];
+         ack = 1;
          end
       end
 
-      else begin
-         if(req != 0 && !wnr) begin
-            data_out <= [address << 1];
-            ack <= 1;
-         end
-         if(req !=0 && wnr) begin
-            mem[address << 1] <= data_in;
-            ack <=1;
-            data_out <=0;
-         end
-         else begin
-            data <=0;
-            ack <=0;
-            end
-      end
    end
 endmodule // register_set
